6df46ad2c520881dcc3b5cd86be20fd066b08d5e
Dani Santos <santos.adaniele@gmail.com>
2021-05-26 20:21:46 -0400

    adds health and metrics endpoints

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,52 +1,114 @@
 import sqlite3
+import redis
+import os
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+import time
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
+
+
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
     return connection
 
 # Function to get a post using its ID
+
+
 def get_post(post_id):
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+                              (post_id,)).fetchone()
     connection.close()
     return post
+
 
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-# Define the main route of the web application 
-@app.route('/')
+
+# Define the main route of the web application
+
+def hits_count():
+    connection = get_db_connection()
+    total = len(connection.execute('SELECT * FROM posts').fetchall())
+    connection.execute('INSERT INTO connections (total) VALUES (?)',
+                       str(total+1))
+    connection.commit()
+    connection.close()
+
+
+def get_total_count():
+    connection = get_db_connection()
+    total = connection.execute('SELECT * FROM connections').fetchall()
+    connection.close()
+    return len(total)
+
+
+@ app.route('/')
 def index():
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
     return render_template('index.html', posts=posts)
 
-# Define how each individual article is rendered 
+
+@ app.route('/healthz')
+def healthcheck():
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+
+    app.logger.info('Status request successfull')
+    return response
+
+
+@ app.route('/metrics')
+def metrics():
+    # Total amount of posts in the database
+    connection = get_db_connection()
+    count = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+    counter = get_total_count()
+    response = app.response_class(
+        response=json.dumps(
+            {"db_connection_count": counter, "post_count": str(len(count))}),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.info('Metrics request successfull')
+    return response
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
-@app.route('/<int:post_id>')
+
+
+@ app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
+
     if post is None:
-      return render_template('404.html'), 404
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        hits_count()
+        return render_template('post.html', post=post)
 
 # Define the About Us page
-@app.route('/about')
+
+
+@ app.route('/about')
 def about():
     return render_template('about.html')
 
-# Define the post creation functionality 
-@app.route('/create', methods=('GET', 'POST'))
+# Define the post creation functionality
+
+
+@ app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
         title = request.form['title']
@@ -57,7 +119,7 @@ def create():
         else:
             connection = get_db_connection()
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+                               (title, content))
             connection.commit()
             connection.close()
 
@@ -65,6 +127,7 @@ def create():
 
     return render_template('create.html')
 
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    app.run(host='0.0.0.0', port='3111')
