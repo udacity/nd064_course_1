841e5adbfb1fc1d63b3a7dbec94ca0f8decf1c7f
FreD <a_fredi@yahoo.com>
2021-06-15 20:27:06 +0800

    all

---
+++ exercises/go-helloworld/go.mod
@@ -0,0 +1,3 @@
+module go-helloworld
+
+go 1.16
--- solutions/python-helloworld/app.py
+++ solutions/python-helloworld/app.py
@@ -29,11 +29,10 @@ def metrics():
 @app.route("/")
 def hello():
     app.logger.info('Main request successfull')
-
     return "Hello World!"
 
 if __name__ == "__main__":
     ## stream logs to a file
-    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+    logging.basicConfig(filename='appsolu.log',level=logging.DEBUG)
     
     app.run(host='0.0.0.0')
