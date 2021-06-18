2c8a8945cbf80cc55aedfdc4a36acbed1fd5d4cc
pk-218 <2182001pk@gmail.com>
2021-06-18 23:59:56 +0530

    Added L2.11 exercise solution

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, json
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/status")
+def status():
+    data = {"user": "admin"}
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route("/metrics")
+def metrics():
+    data = {"UserCount": 140, "UserCountActive": 23}
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
