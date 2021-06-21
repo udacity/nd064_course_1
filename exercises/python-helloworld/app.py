201a53b1b4d9471fb6102a531c657aa43b117ea4
thiagofis <Th516532>
2021-06-21 22:54:05 +0100

    1st exercise

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,25 @@
+import logging
+
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    app.logger.info('Hello request successfull')
     return "Hello World!"
 
+
+@app.route("/status")
+def status():
+    app.logger.info('Status request successfull')
+    return {"result": "OK - healthy"}
+
+@app.route("/metrics")
+def metrics():
+    app.logger.info('Metrics request successfull')
+    return {"UserCount": 140, "UserCountActive": 23}
+
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
     app.run(host='0.0.0.0')
