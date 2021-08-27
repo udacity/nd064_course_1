ef856e14ea59fd07e939a70a612b951e69b8df16
Jung Soon Noh <gracewm892@gmail.com>
2021-08-27 12:12:00 +0900

    update app.py, add Dockerfile, add techtrends-dockerhub

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,7 +1,14 @@
 import sqlite3
+import os
+import logging
+from datetime import datetime
+import sys
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
+now = datetime.now()
+timestamp = now.strftime("%m/%d/%Y, %H:%M:%S")
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
@@ -10,39 +17,55 @@ def get_db_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
+conn = get_db_connection()
+db_conn = 0
+if conn:
+    db_conn +=1
+
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+                        (post_id,)).fetchone() 
+    if post:                    
+        app.logger.info('%s, Article "%s" retrieved!', timestamp, post['title']) 
+    else:
+        app.logger.info('%s, Article Not Found!', timestamp)
+        
     connection.close()
-    return post
-
+    return post      
+ 
 # Define the Flask application
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your secret key'
+app.config['SECRET_KEY'] = os.urandom(24)
 
 # Define the main route of the web application 
 @app.route('/')
 def index():
-    connection = get_db_connection()
+    connection = get_db_connection()    
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
     return render_template('index.html', posts=posts)
+
+connection = get_db_connection()    
+posts = connection.execute('SELECT * FROM posts').fetchall()
+num_posts = len(posts)
 
 # Define how each individual article is rendered 
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
-    post = get_post(post_id)
+    post = get_post(post_id)   
+    
     if post is None:
-      return render_template('404.html'), 404
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('%s, "About Us" page retrieved!', timestamp)
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -51,7 +74,7 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-
+        
         if not title:
             flash('Title is required!')
         else:
@@ -59,12 +82,37 @@ def create():
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
                          (title, content))
             connection.commit()
+            app.logger.info('%s, Article "%s" created!', timestamp, title)
             connection.close()
 
             return redirect(url_for('index'))
-
+        
     return render_template('create.html')
 
+@app.route('/healthz')
+def healthcheck():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    app.logger.info('Health Check request successful')
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success", "data": {"db_connection_count": db_conn , "post_count": num_posts}}),
+            status=200,
+            mimetype='application/json'
+    )    
+    app.logger.info('Metrics request successful')
+    return response
+
 # start the application on port 3111
-if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+if __name__ == "__main__":  
+    # logging.basicConfig(filename='app.log',level=logging.DEBUG) 
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+        
+    app.run(host='0.0.0.0', port='3111')
