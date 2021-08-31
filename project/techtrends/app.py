4a331eb8e09bd84fe271887c27e6fc00eeeb88e8
Andrea Gutierrez Quintanilla <38392931+agutierrezq@users.noreply.github.com>
2021-08-30 20:17:36 -0500

    Commit #1
    Add /helthz, /metrics, global variable DbConnectionCount + posts.
    Dude: the DbConnectionCount's declaration is true?

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -8,6 +8,7 @@ from werkzeug.exceptions import abort
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    DbConnectionCount += 1
     return connection
 
 # Function to get a post using its ID
@@ -25,7 +26,9 @@ app.config['SECRET_KEY'] = 'your secret key'
 # Define the main route of the web application 
 @app.route('/')
 def index():
+    global DbConnectionCount
     connection = get_db_connection()
+    # DbConnectionCount += 1
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
     return render_template('index.html', posts=posts)
@@ -68,3 +71,40 @@ def create():
 # start the application on port 3111
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port='3111')
+
+# Healthcheck endpoint
+# Build the /healthz endpoint for the TechTrends application. The endpoint should return the following response:
+# An HTTP 200 status code
+# A JSON response containing the result: OK - healthy message
+@app.route('/healthz')
+def healthcheck():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info('Status request successfull')
+    return response
+
+
+# Metrics endpoint
+# Build a /metrics endpoint that would return the following:
+# An HTTP 200 status code
+# A JSON response with the following metrics:
+# Total amount of posts in the database
+# Total amount of connections to the database.
+# For example, accessing an article will query the database, hence will count as a connection.
+# Example output: {"db_connection_count": 1, "post_count": 7}
+# Tips: The /metrics endpoint response should NOT be hardcoded.
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"db_connection_count": 1, "post_count": connection.execute('SELECT * FROM posts').fetchall()}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    app.logger.info('Metrics request successfull')
+    return response
+
