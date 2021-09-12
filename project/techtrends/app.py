059a36a62801c7644f307a6885c420e69993a425
Andrea Gutierrez Quintanilla <38392931+agutierrezq@users.noreply.github.com>
2021-09-11 22:11:40 -0500

    Step #1 finish

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -16,6 +16,9 @@ def get_db_connection():
     connection = sqlite3.connect('D:\\Udacity\\Nanodegree\\project\\techtrends\\database.db')
     DbConnectionCount +=1
     connection.row_factory = sqlite3.Row
+
+    # Log Message
+    app.logger.info('DB connection successfull')
     return connection
 
 
@@ -27,6 +30,9 @@ def get_post(post_id):
                               (post_id,)).fetchone()
     postsCounts += 1
     connection.close()
+
+    # Log Message
+    app.logger.info('Get a post {0} successfull'.format(post['id']))
     return post
 
 # Define the Flask application
@@ -37,10 +43,11 @@ app.config['SECRET_KEY'] = 'your secret key'
 # Define the main route of the web application
 @app.route('/')
 def index():
-    global postsCounts
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
+    # Log Message
+    app.logger.info('Index page successfull')
     return render_template('index.html', posts=posts)
 
 
@@ -50,6 +57,8 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+        # Log Message
+        app.logger.info('Post not found - 404 error')
         return render_template('404.html'), 404
     else:
         # Log Message
@@ -67,12 +76,18 @@ def about():
 # Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
+    # Log Message
+    app.logger.info('Post creation started')
+
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
 
         if not title:
             flash('Title is required!')
+
+            # Log Message
+            app.logger.info('Error: Title is required!')
         else:
             connection = get_db_connection()
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
@@ -80,8 +95,12 @@ def create():
             connection.commit()
             connection.close()
 
+            # Log Message
+            app.logger.info('Post creation successfull')
             return redirect(url_for('index'))
 
+    # Log Message
+    app.logger.info('Warning')
     return render_template('create.html')
 
 
