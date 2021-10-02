233d49d0c9f91cc3d42d585776500e10307912f0
Vincent <vincentbiasa6767@gmail.com>
2021-10-02 16:39:54 +0700

    Add healthz endpoint

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -65,6 +65,11 @@ def create():
 
     return render_template('create.html')
 
+# Define the healthz endpoint
+@app.route('/healthz')
+def returnHealthz():
+    return jsonify({"status": "OK - healthy"})
+
 # start the application on port 3111
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port='3111')
