1e7d67f3ea941552f787215627c36ce22172b6d6
Vincent <vincentbiasa6767@gmail.com>
2021-10-02 21:07:37 +0700

    Add info logs

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,4 +1,5 @@
 from os import cpu_count
+import logging
 import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
@@ -42,13 +43,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        app.logger.info("Article with post ID '%s' not found!", post_id)
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        app.logger.info("Article '%s' retrieved!", post['title'])
+        return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info("About article accessed")
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -66,9 +70,8 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            app.logger.info("Created new article!")
             return redirect(url_for('index'))
-
     return render_template('create.html')
 
 # Define the healthz endpoint
@@ -85,6 +88,12 @@ def return_metrics():
     return jsonify({"posts_count": posts_count['COUNT(*)'], "db_connection_count": conn_counter})
 
 
+
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        datefmt="%Y-%m-%d %H:%M:%S", 
+        level=logging.DEBUG)
+    app.run(host='0.0.0.0', port='3111')
