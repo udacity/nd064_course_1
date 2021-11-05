8a69f7d20b25978c8eb19f2952c32646cb80c753
Eduard Schildner <eduard.schildner@gmail.com>
2021-11-05 23:39:23 +0100

    excersise 1 - add healthchecks

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,28 @@
 from flask import Flask
+from flask import json
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/status")
+def healthcheck():
+    response = app.response_class(
+        response=json.dumps({"result":"OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+        response=json.dumps({"data":{"UserCount":140,"UserCountActive":23}}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
