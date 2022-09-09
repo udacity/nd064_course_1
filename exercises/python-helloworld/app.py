8868e0949d804b7f88ca1b7274b522cbf285d2d1
krishnamohan-yerrabilli <krishnamohan72452964@gmail.com>
2022-09-09 11:16:22 +0530

    added metrics and status

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,5 +1,32 @@
+from email.mime import application
+import json
+import mimetypes
+from urllib import response
 from flask import Flask
 app = Flask(__name__)
+
+
+
+@app.route('/status')
+def status():
+    response = app.response_class( 
+            response=json.dumps({"result":"OK - healthy"}), #json.dumps() converts python object it a json string
+            status=200, #this refers to the http requests has been succesfully served
+            mimetype='application/json' #format of the response data 
+    )
+
+    return response
+    
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class( 
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount": 140, "UserCountActive": 23}}), #json.dumps() converts python object it a json string
+            status=200, #this refers to the http requests has been succesfully served
+            mimetype='application/json' #format of the response data 
+    )
+
+    return response
 
 @app.route("/")
 def hello():
