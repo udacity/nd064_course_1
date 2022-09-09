16b56cb61a97129da85b2c65eb9b9c68b5e035de
krishnamohan-yerrabilli <krishnamohan72452964@gmail.com>
2022-09-09 10:18:10 +0530

    added metrics and server status to the application

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -5,5 +5,16 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/status")
+def status():
+    return "result: OK - healthy"
+
+
+@app.route("/metrics")
+def metrics():
+    return "data: {UserCount: 140, UserCountActive: 23}"
+
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
