470296a47d9817c5e0ffe2a7a50c3b7860b831b6
Przemyslaw Sulecki <przemyslaw.sulecki@nokia.com>
2022-10-10 11:59:16 +0200

    [FIX] Log to stdout instead off a file

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -112,5 +112,5 @@ def metrics():
 # start the application on port 3111
 if __name__ == "__main__":
     FORMAT = "%(asctime)s %(message)s"
-    logging.basicConfig(format=FORMAT, filename="app.log", level=logging.DEBUG)
+    logging.basicConfig(format=FORMAT, level=logging.DEBUG)
     app.run(host="0.0.0.0", port="3111")
