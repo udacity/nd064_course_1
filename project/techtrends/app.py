f7ac3e0524c27d15de0b476e217cd20c3fd0e584
Singh, Jay <Jay.Singh@philips.com>
2022-10-29 19:21:22 +0530

    initial commit

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,7 +1,14 @@
+from multiprocessing import connection
 import sqlite3
+import logging
+import sys
+from tkinter import W
+from turtle import screensize
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
+logger = None
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
@@ -36,13 +43,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
-    else:
-      return render_template('post.html', post=post)
+        logger.info("Article for post id \"{}\" does not exist! a 404 page is returned".format(post_id))
+        return render_template('404.html')        
+    else:        
+        logger.info("Article \"{}\" retrieved!".format(post['title']))
+        return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    logger.info("The \"About Us\" page is retrieved.")
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -61,10 +71,58 @@ def create():
             connection.commit()
             connection.close()
 
+            logger.info("Article \"{}\" is created.".format(title))
+            
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+@app.route('/healthz')
+def healthz():
+    logger.info("healthz called")
+    response = app.response_class(
+    response = json.dumps({'result' : 'Ok - healthy'}), 
+    status = 200,
+    mimetype="application/json"
+    )
+    return response
+
+@app.route('/metrics')
+def metrics():
+    logger.info("metrics called")   
+    
+    connection = get_db_connection()
+    post_count = connection.execute('SELECT count(*) FROM posts').fetchone()
+    tables = connection.execute('SELECT name FROM sqlite_master WHERE type=\'table\'').fetchall()  
+    response = app.response_class(
+    response = json.dumps({"db_connection_count": 1, "post_count": post_count[0]}), 
+    status = 200,
+    mimetype="application/json"    
+    )
+
+    connection.close()
+    return response
+
+
+def setup_logger():    
+    #logging.basicConfig(filename='app.log',level=logging.DEBUG, format='%(levelname)s %(asctime)-8s %(message)s', datefmt='%d/%m/%Y, %H:%M:%S')   
+    formatter = logging.Formatter(fmt='%(levelname)s %(asctime)-8s %(message)s', datefmt='%d/%m/%Y, %H:%M:%S')
+    handler = logging.FileHandler('tech_trend_log.txt')
+    handler.setFormatter(formatter)
+    
+    screen_handler = logging.StreamHandler(stream=sys.stdout)
+    screen_handler.setFormatter(formatter)
+
+    logger_temp = logging.getLogger("app")
+    logger_temp.setLevel(logging.INFO)    
+    logger_temp.addHandler(handler)
+    logger_temp.addHandler(screen_handler)
+    return logger_temp
+
 # start the application on port 3111
 if __name__ == "__main__":
+    
+   #logging.basicConfig(filename='app.log',level=logging.DEBUG, format='%(levelname)s %(modulename)s %(asctime)-8s %(message)s', datefmt='%d/%m/%Y, %H:%M:%S')   
+   #global logger
+   logger = setup_logger()
    app.run(host='0.0.0.0', port='3111')
