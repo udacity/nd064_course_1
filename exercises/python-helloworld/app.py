bb0f7d8070c6a5c999b63ab7e4747dca210ff74a
Thitiwat Watanajaturaporn <thitiwat@fb.com>
2023-10-07 16:29:13 -0700

    add status and metrics endpoint

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,5 +1,33 @@
 from flask import Flask
+import json
+
 app = Flask(__name__)
+
+@app.route("/status")
+def status():
+    response = app.response_class(
+        response=json.dumps({"result": "OK - Healthy"}),
+        status=200,
+        mimetype="application/json"
+    )
+    return response
+
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+        response=json.dumps({
+            "status": "success",
+            "code": 0,
+            "data": {
+                "userCount": 140,
+                "userCountActive": 23
+            }
+        }),
+        status=200,
+        mimetype="application/json"
+    )
+    return response
+
 
 @app.route("/")
 def hello():
