9665b764b782cae1de895a53c507d96fba162451
Thitiwat Watanajaturaporn <thitiwat@fb.com>
2023-10-07 17:18:44 -0700

    Add basic logging mechanism

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,15 +1,18 @@
 from flask import Flask
 import json
+import logging
 
 app = Flask(__name__)
 
 @app.route("/status")
-def status():
+def healthcheck():
     response = app.response_class(
         response=json.dumps({"result": "OK - Healthy"}),
         status=200,
         mimetype="application/json"
     )
+    ## log line
+    app.logger.info('Status request successfull')
     return response
 
 @app.route("/metrics")
@@ -26,12 +29,18 @@ def metrics():
         status=200,
         mimetype="application/json"
     )
+    ## log line
+    app.logger.info('Metrics request successfull')
     return response
 
 
 @app.route("/")
 def hello():
+    # Logging a custom message
+    app.logger.info("Main request successfull")
     return "Hello World!"
 
 if __name__ == "__main__":
+    # Stream logs to a file, and set the default log level to DEBUG
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0')
